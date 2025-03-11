0662cdc15c6a7f6cd57cf477804ac46da7ae6a09
WERTYUIOKL <147541259+WERTYUIOKL@users.noreply.github.com>
2025-03-11 12:47:43 +0530

    openmp_matrix_mult.cpp

--- openmp_matrix_mult.cpp
+++ openmp_matrix_mult.cpp
@@ -7,11 +7,10 @@
 using namespace std;
 using namespace std::chrono;
 
-const int N = 100; // matrix size
+const int N = 100; 
 
 void initialize_matrix(int matrix[N][N])
 {
-    // initialize matrix with random values
     srand(time(NULL));
     for (int i = 0; i < N; i++)
     {
@@ -24,8 +23,6 @@ void initialize_matrix(int matrix[N][N])
 
 void multiply_matrices(int A[N][N], int B[N][N], int C[N][N])
 {
-// perform matrix multiplication in parallel using OpenMP
-#pragma omp parallel for
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
@@ -42,19 +39,15 @@ void multiply_matrices(int A[N][N], int B[N][N], int C[N][N])
 int main()
 {
     int A[N][N], B[N][N], C[N][N];
-
-    // initialize matrices A and B with random values
+    
     initialize_matrix(A);
     initialize_matrix(B);
 
-    // calculate execution time
     auto start_time = high_resolution_clock::now();
-    // perform matrix multiplication
     multiply_matrices(A, B, C);
     auto stop_time = high_resolution_clock::now();
     auto duration = duration_cast<nanoseconds>(stop_time - start_time);
 
-    // print execution time
     cout << "Execution time: " << duration.count() << " nanoseconds\n";
 
     return 0;
